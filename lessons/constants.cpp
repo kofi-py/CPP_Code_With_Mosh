86b0fc132d5553932f4b68a967006c9459a91b4f
Kofi <pastorkofi101@gmail.com>
2023-06-14 16:37:59 -0400

    Lesson 2: Constants

---
+++ lessons/constants.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    // Lesson 2: Constants
+    // Whenever you need to use a constant, put the const
+    // keyword in front of the variable type declaration.
+    const double pi = 3.14;
+    return 0;
+}
