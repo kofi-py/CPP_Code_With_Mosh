9247961e02d3037d65e732d2fd85c44a33a63912
Kofi <pastorkofi101@gmail.com>
2023-06-14 16:33:56 -0400

    variables lesson.

---
+++ lessons/variables.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Lesson 1: Variables
+    int file_size = 100;
+    double sales = 9.99;
+    // Always make your variable names meaningful!
+    std::cout << file_size;
+}
